2f68cc1e4b7d889df60826d1d226fe2dda9448eb
nahee09 <tink159976@gmail.com>
2023-01-12 11:38:56 +0900

    작은 이미지를 클릭하면 큰 이미지로 나타내기. alt값도 바꾸기.

--- client/js/part01.js
+++ client/js/part01.js
@@ -1,6 +1,6 @@
+//이벤트 위임
 
-
-
+//data는 배열로 만들어줬네. 
 const data = [
   {
     "id": 1,
@@ -33,17 +33,24 @@ const list = getNodes('.navigation > li');
 function handler(e){
   //이벤트 대상에 가장 인접한 li를 찾는다. 
   let target = e.target.closest('li');
-  let list = makeArray(navigation.children); 
-
   //li 사이 빈 공간 누르면 null이 반환 돼서 그럼 종료되게. 
   if(!target) return; 
+
+  let list = makeArray(navigation.children); 
+  let index = attr(target, 'data-index');
 
   //내가 선택하지 않은(모든 li) li에게 is-active 클래스를 제거. 
   list.forEach(item => removeClass(item, 'is-active'));
 
   //내가 선택한 li에게 is-active 클래스를 넣기 
   addClass(target, 'is-active');
-  
+
+  //이미지명의 숫자를 바뀌게. 
+  //비주얼 안에 있는 이미지를 가져오기 
+  let visualImg = getNode('.visual img');
+  //이미지의 src 속성에 접근해서 src값을 index로 바꾸기
+  attr(visualImg, 'src', `./assets/part01/${data[index-1].src}`);
+  attr(visualImg, 'alt', data[index-1].alt); 
 }
 
 navigation.addEventListener('click', handler);
