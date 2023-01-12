63f3ebef4af9c966d056511ed1188482123f37ef
nahee09 <tink159976@gmail.com>
2023-01-12 15:01:29 +0900

    swiper api 써보기~

--- client/js/part03.js
+++ client/js/part03.js
@@ -23,6 +23,29 @@ const data = [
 ]
 
 
+const swiper = new Swiper('.swiper', {
+  autoplay: true, 
+  loop: true,  
+  speed: 2000,
+  pagination:{
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletClass: 'bullet', 
+    bulletActiveClass: 'is-active', 
+    renderBullet: function(index, className){
+      return /*html*/ `
+        <span class=${className}>
+          <img src="./assets/part01/${data[index].src}" 
+          alt="${data[index].alt}"/>
+        </span>
+      `
+    }
+  },
+  parallax: true
+});
+
+
+
 
 
 
